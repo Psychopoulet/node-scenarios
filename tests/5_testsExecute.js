d197f2522dcc01c58b159c2ae6833ffc80a7ffcb
Sébastien Vidal <Sbastien Vidal>
2016-06-14 12:23:41 +0200

    finition des tests de génération de way

--- tests/5_testsExecute.js
+++ tests/5_testsExecute.js
@@ -2,7 +2,8 @@
 
 // deps
 
-	const SimpleScenarios = require(require("path").join(__dirname, "..", "lib", "main.js"));
+	const	assert = require("assert"),
+			SimpleScenarios = require(require("path").join(__dirname, "..", "lib", "main.js"));
 
 // tests
 
@@ -95,9 +96,47 @@ describe("execute", function() {
 				return container.get("scenarios").last();
 			}).then(function(scenario) {
 				return container.get("scenarios").getWay(scenario);
-			}).then(function(way) {
+			}).then(function(scenario) {
 
-				console.log(way);
+				assert.strictEqual("scenario", scenario.nodetype, "Scenario returned is not valid (nodetype)");
+				assert.strictEqual("test", scenario.name, "Scenario returned is not valid (name)");
+				assert.strictEqual(true, scenario.active, "Scenario returned is not valid (active)");
+
+				assert.strictEqual("object", typeof scenario.start, "Scenario returned is not valid (start)");
+
+					assert.strictEqual("condition", scenario.start.nodetype, "Scenario returned is not valid (start nodetype)");
+					assert.strictEqual("true", scenario.start.name, "Scenario returned is not valid (start name)");
+					assert.strictEqual("true", scenario.start.value, "Scenario returned is not valid (start name)");
+					
+					assert.strictEqual("object", typeof scenario.start.type, "Scenario returned is not valid (start type)");
+
+						assert.strictEqual("EQUAL", scenario.start.type.code, "Scenario returned is not valid (start type code)");
+						assert.strictEqual("equal", scenario.start.type.name, "Scenario returned is not valid (start type name)");
+
+					assert.strictEqual("object", typeof scenario.start.onyes, "Scenario returned is not valid (start onyes)");
+
+						assert.strictEqual("action", scenario.start.onyes.nodetype, "Scenario returned is not valid (start onyes nodetype)");
+						assert.strictEqual("console yes", scenario.start.onyes.name, "Scenario returned is not valid (start onyes name)");
+						assert.strictEqual("yes", scenario.start.onyes.params, "Scenario returned is not valid (start onyes params)");
+						assert.strictEqual(null, scenario.start.onyes.after, "Scenario returned is not valid (start onyes after)");
+
+						assert.strictEqual("object", typeof scenario.start.onyes.type, "Scenario returned is not valid (start onyes type)");
+
+							assert.strictEqual("CONSOLE", scenario.start.onyes.type.code, "Scenario returned is not valid (start onyes type code)");
+							assert.strictEqual("console", scenario.start.onyes.type.name, "Scenario returned is not valid (start onyes type name)");
+
+					assert.strictEqual("object", typeof scenario.start.onno, "Scenario returned is not valid (start onno)");
+
+						assert.strictEqual("action", scenario.start.onno.nodetype, "Scenario returned is not valid (start onno nodetype)");
+						assert.strictEqual("console no", scenario.start.onno.name, "Scenario returned is not valid (start onno name)");
+						assert.strictEqual("no", scenario.start.onno.params, "Scenario returned is not valid (start onno params)");
+						assert.strictEqual(null, scenario.start.onno.after, "Scenario returned is not valid (start onno after)");
+
+						assert.strictEqual("object", typeof scenario.start.onno.type, "Scenario returned is not valid (start onno type)");
+
+							assert.strictEqual("CONSOLE", scenario.start.onno.type.code, "Scenario returned is not valid (start onno type code)");
+							assert.strictEqual("console", scenario.start.onno.type.name, "Scenario returned is not valid (start onno type name)");
+
 				done();
 
 			}).catch(done);
